76549f8f744088be93da0b5856fe976ea3b3403d
Roby <changkaizhao1006@gmail.com>
2020-09-05 11:11:11 +0800

    增加69题

---
+++ 6和9组成的最大数字/main.py
@@ -0,0 +1,14 @@
+# 题目链接 https://leetcode-cn.com/problems/maximum-69-number/
+
+
+def test(num):
+    # 逻辑代码写到这里
+    return
+
+
+if __name__ == "__main__":
+    test_nums = [6699, 9696,   69696969, 66669696,
+                 9966969, 696, 99969, 999, 6666, 9996, 999996666]
+
+    for n in test_nums:
+        print(test(n))
